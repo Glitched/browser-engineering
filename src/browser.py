5258216098b2e2da38ca1cf3a48a8fc4bdb15134
Ryan Slama <ryanmslama@gmail.com>
2024-11-22 13:18:22 -0500

    2.3

--- src/browser.py
+++ src/browser.py
@@ -4,9 +4,9 @@ from typing import TYPE_CHECKING
 from entities import entities
 from url import URL
 
-WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 PARAGRAPH_STEP = 2 * VSTEP
+SCROLL_STEP = 100
 
 # Typeshed definitions require generics, but we can't subscript python builtins
 if TYPE_CHECKING:
@@ -19,19 +19,29 @@ class Browser:
     window: tkinter.Tk
     canvas: tkinter.Canvas
     scroll: int
+    text: str
     display_list: list[tuple[int, int, str]]
+    width: int
+    height: int
 
     def __init__(self):
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-        self.canvas.pack()
+        self.window.title("Slama Browser")
+        self.window.resizable(True, True)
+        self.width = 800
+        self.height = 600
+        self.canvas = tkinter.Canvas(self.window, width=self.width, height=self.height)
+        self.canvas.pack(expand=True, fill="both")
         self.scroll = 0
+        self.window.bind("<Up>", self.scrollup)
         self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<MouseWheel>", self.scrollwheel)
+        self.window.bind("<Configure>", self.on_configure)
 
     def load(self, url: URL):
         body = url.request()
         text = self.lex(body)
-        self.layout(text)
+        self.layout()
         self.draw()
 
     def lex(self, body: str):
@@ -48,12 +58,13 @@ class Browser:
         for entity, char in entities.items():
             output = output.replace(entity, char)
 
-        return output
+        self.text = output
 
-    def layout(self, text: str):
+    def layout(self):
         display_list: list[tuple[int, int, str]] = []
         cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
+
+        for c in self.text:
             # Support line breaks
             if c == "\n":
                 cursor_x = HSTEP
@@ -62,7 +73,7 @@ class Browser:
 
             display_list.append((cursor_x, cursor_y, c))
             cursor_x += HSTEP
-            if cursor_x + HSTEP > WIDTH:
+            if cursor_x + HSTEP > self.width:
                 cursor_x = HSTEP
                 cursor_y += VSTEP
 
@@ -73,14 +84,30 @@ class Browser:
 
         for x, y, c in self.display_list:
             # Skip drawing characters off screen
-            if y > self.scroll + HEIGHT:
+            if y > self.scroll + self.height:
                 continue
             if y + VSTEP < self.scroll:
                 continue
 
             self.canvas.create_text(x, y - self.scroll, text=c)
 
+    def on_configure(self, e: EventType):
+        self.width = e.width
+        self.height = e.height
+        self.layout()
+        self.draw()
+
+    def add_scroll(self, offset: int):
+        self.scroll = max(0, self.scroll + offset)
+
     def scrolldown(self, e: EventType):
-        SCROLL_STEP = 100
-        self.scroll += SCROLL_STEP
+        self.add_scroll(SCROLL_STEP)
+        self.draw()
+
+    def scrollup(self, e: EventType):
+        self.add_scroll(-SCROLL_STEP)
+        self.draw()
+
+    def scrollwheel(self, e: EventType):
+        self.add_scroll(e.delta * (SCROLL_STEP // 2))
         self.draw()
