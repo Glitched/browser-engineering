94298c05d1c831b98b69788a0994af3f852daae5
Ryan Slama <ryanmslama@gmail.com>
2024-11-21 22:21:57 -0500

    Ch 1 exercises 6 - 9; Done!

--- src/url.py
+++ src/url.py
@@ -1,7 +1,22 @@
+import gzip
 import socket
 import ssl
+import time
+from dataclasses import dataclass
 
 from entities import entities
+
+# (host, port) -> socket
+sockets: dict[tuple[str, int], socket.socket] = {}
+
+
+@dataclass
+class CacheEntry:
+    content: str
+    expires: float
+
+
+cache: dict[str, CacheEntry] = {}
 
 
 class URL:
@@ -12,13 +27,14 @@ class URL:
     view_source: bool = False
     headers: dict[str, str] = {
         "User-Agent": "Slama/0.1",
-        # "Connection": "Close",
+        "Connection": "keep-alive",
+        "Accept-Encoding": "gzip",
     }
+    redirects: list[str]
 
-    # (host, port) -> socket
-    sockets: dict[tuple[str, int], socket.socket] = {}
+    def __init__(self, url: str, redirects: list[str] = list()):
+        self.redirects = redirects
 
-    def __init__(self, url: str):
         if url.startswith("view-source:"):
             self.view_source = True
             url = url[12:]
@@ -44,7 +60,17 @@ class URL:
             self.host, port = self.host.split(":", 1)
             self.port = int(port)
 
-    def request(self):
+    def get_url_string(self) -> str:
+        return f"{self.scheme}://{self.host}:{self.port}{self.path}"
+
+    def request(self) -> str:
+        if self.get_url_string() in cache:
+            entry = cache[self.get_url_string()]
+            if entry.expires > time.time():
+                return entry.content
+            else:
+                del cache[self.get_url_string()]
+
         if self.scheme == "file":
             with open(self.path, "rb") as f:
                 return f.read().decode("utf8")
@@ -61,23 +87,58 @@ class URL:
         request = self.build_request()
         s.send(request.encode("utf8"))
 
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-        statusline = response.readline()
-        _version, _status, _explanation = statusline.split(" ", 2)
+        response = s.makefile("rb")
+        statusline = response.readline().decode("utf8")
+        _version, status, _explanation = statusline.split(" ", 2)
 
         response_headers: dict[str, str] = {}
         while True:
-            line = response.readline()
+            line = response.readline().decode("utf8")
             if line == "\r\n":
                 break
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
+        content_encoding = response_headers.get("content-encoding", "")
+        transfer_encoding = response_headers.get("transfer-encoding", "")
+        if transfer_encoding == "chunked":
+            body_bytes = b""
+            while True:
+                chunk_length = int(response.readline().decode("utf8"), 16)
+                body_bytes += response.read(chunk_length)
+                response.read(2)
+                if chunk_length == 0:
+                    break
+        else:
+            content_length = int(response_headers["content-length"])
+            body_bytes = response.read(content_length)
 
-        content_length = int(response_headers.get("content-length", "0"))
-        content = response.read(content_length)
+        if content_encoding == "gzip":
+            content = gzip.decompress(body_bytes).decode("utf8")
+        else:
+            content = body_bytes.decode("utf8")
+
+        if status == "301" or status == "302":
+            location = response_headers["location"]
+            if location in self.redirects:
+                raise Exception("Redirect loop")
+            if len(self.redirects) > 10:
+                raise Exception("Too many redirects")
+
+            if location.startswith("/"):
+                location = f"{self.scheme}://{self.host}:{self.port}{location}"
+
+            return URL(location, self.redirects + [location]).request()
+
+        cache_control = response_headers.get("cache-control", "")
+        if (
+            status == "200"
+            and "no-store" not in cache_control
+            and "no-cache" not in cache_control
+            and "max-age" in cache_control
+        ):
+            max_age = int(cache_control.split("=")[1])
+            cache[self.get_url_string()] = CacheEntry(content, time.time() + max_age)
 
         if self.view_source:
             for entity, char in entities.items():
@@ -94,8 +155,8 @@ class URL:
         return request
 
     def get_socket(self):
-        if (self.host, self.port) in self.sockets:
-            return self.sockets[(self.host, self.port)]
+        if (self.host, self.port) in sockets:
+            return sockets[(self.host, self.port)]
 
         s = socket.socket(
             family=socket.AF_INET,
@@ -104,6 +165,6 @@ class URL:
         )
 
         s.connect((self.host, self.port))
-        self.sockets[(self.host, self.port)] = s
+        sockets[(self.host, self.port)] = s
 
         return s
